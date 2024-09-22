a3e27b8b1da255a9c197f3949f25f43f4f8fd49b
charlesjin123 <charlesjin123@gmail.com>
2024-09-22 00:45:31 -0400

    tab icons and fixed volume

--- frontend/app/(tabs)/chat.tsx
+++ frontend/app/(tabs)/chat.tsx
@@ -83,7 +83,7 @@ const Chat = () => {
 
       console.log('Sending audio to backend...');
 
-      const response = await axios.post('http://10.102.103.117:5000/process_audio', formData, {
+      const response = await axios.post('http://10.102.79.218:5000/process_audio', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
